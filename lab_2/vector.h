ca247f2cdb856ca1754d9aaf7be792767d0e3718
Makhdi1 <makhdisaf@gmail.com>
2026-03-13 10:49:08 +0300

    Update vector.h

--- lab_2/vector.h
+++ lab_2/vector.h
@@ -6,30 +6,28 @@
 #include <algorithm>
 #include <utility>
 
+
 template <class T>
 class vector
 {
 public:
-    using value_type = T;
-    using iterator = T*;
-    using const_iterator = const T*;
-    using reference = T&;
-    using const_reference = const T&;
-    using size_type = size_t;
-    using difference_type = ptrdiff_t;
-
-    vector() : m_data(nullptr), m_size(0), m_capacity(0) {}
-
-    vector(size_type n, const T& value = T())
+    using value_type =T;
+    using iterator =T*;
+    using const_iterator= const T*;
+    using reference =T&;
+    using const_reference= const T&;
+    using size_type =size_t;
+    using difference_type= ptrdiff_t;
+    vector() : m_data(nullptr),m_size(0),m_capacity(0) {}
+    vector(size_type n,const T& value = T())
         : m_data(nullptr), m_size(0), m_capacity(0)
     {
         if (n > 0) {
             reserve(n);
-            for (size_type i = 0; i < n; ++i)
+            for (size_type i =0;i < n;++i)
                 push_back(value);
         }
     }
-
     vector(const vector& other)
         : m_data(nullptr), m_size(0), m_capacity(0)
     {
@@ -37,7 +35,6 @@ public:
         for (const auto& elem : other)
             push_back(elem);
     }
-
     ~vector()
     {
         clear();
@@ -50,36 +47,40 @@ public:
             vector tmp(other);
             swap(tmp);
         }
+        
         return *this;
     }
 
     reference operator[](size_type index) { return m_data[index]; }
     const_reference operator[](size_type index) const { return m_data[index]; }
-
     reference at(size_type index)
     {
-        if (index >= m_size) throw std::out_of_range("at");
+        if (index >=m_size) throw std::out_of_range("at");
         return m_data[index];
     }
     const_reference at(size_type index) const
     {
-        if (index >= m_size) throw std::out_of_range("at");
+        
+        if (index >=m_size) throw std::out_of_range("at");
         return m_data[index];
     }
 
+
     reference front()
     {
-        if (empty()) throw std::out_of_range("front");
+        if (empty()) throw std::out_of_range(" front ");
         return m_data[0];
     }
     const_reference front() const
     {
-        if (empty()) throw std::out_of_range("front");
+        if (empty()) throw std::out_of_range(" front");
         return m_data[0];
+        
     }
 
     reference back()
     {
+        
         if (empty()) throw std::out_of_range("back");
         return m_data[m_size-1];
     }
@@ -89,13 +90,14 @@ public:
         return m_data[m_size-1];
     }
 
+
     T* data() { return m_data; }
     const T* data() const { return m_data; }
-
     iterator begin() { return m_data; }
     const_iterator begin() const { return m_data; }
     iterator end() { return m_data + m_size; }
     const_iterator end() const { return m_data + m_size; }
+
 
     bool empty() const { return m_size == 0; }
     size_type size() const { return m_size; }
@@ -110,24 +112,26 @@ public:
             for (; i < m_size; ++i)
                 new (new_data + i) T(m_data[i]);
         } catch (...) {
+            
             for (size_type j = 0; j < i; ++j)
                 (new_data + j)->~T();
             ::operator delete(new_data);
             throw;
         }
+        
         for (size_type j = 0; j < m_size; ++j)
             (m_data + j)->~T();
         ::operator delete(m_data);
         m_data = new_data;
         m_capacity = new_cap;
     }
-
     void clear()
     {
         for (size_type i = 0; i < m_size; ++i)
             (m_data + i)->~T();
         m_size = 0;
     }
+
 
     iterator insert(iterator pos, const T& value)
     {
@@ -150,6 +154,7 @@ public:
         return begin() + idx;
     }
 
+
     void push_back(const T& value)
     {
         if (m_size == m_capacity) {
@@ -166,6 +171,7 @@ public:
         --m_size;
         (m_data + m_size)->~T();
     }
+
 
     void swap(vector& other)
     {
